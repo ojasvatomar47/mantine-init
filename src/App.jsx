e1682c568917bcc6af612366be82da1d17e4a7ef
Ojasva Tomar <ojasvatomar47@gmail.com>
2024-10-17 10:44:56 +0530

    Mantine init

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import '@mantine/core/styles.css';
+import { LineChart } from '@mantine/charts';
+import { MantineProvider } from '@mantine/core';
 
-function App() {
-  const [count, setCount] = useState(0)
+const data = [
+  { name: 'Page A', uv: 4000, pv: 2400 },
+  { name: 'Page B', uv: 3000, pv: 1398 },
+  { name: 'Page C', uv: 2000, pv: 9800 },
+  { name: 'Page D', uv: 2780, pv: 3908 },
+  { name: 'Page E', uv: 1890, pv: 4800 },
+  { name: 'Page F', uv: 2390, pv: 3800 },
+  { name: 'Page G', uv: 3490, pv: 4300 },
+];
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <MantineProvider>
+      <LineChart
+        h={300}
+        data={data}
+        dataKey="name"
+        withRightYAxis
+        yAxisLabel="uv"
+        rightYAxisLabel="pv"
+        series={[
+          { name: 'uv', color: 'pink.6' },
+          { name: 'pv', color: 'cyan.6', yAxisId: 'right' },
+        ]}
+      />
+    </MantineProvider>
   )
 }
 
